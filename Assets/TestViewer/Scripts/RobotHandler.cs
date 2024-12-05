358c98b26c75aa6c44d3ece225bb267cdf72b6ab
Eddie <eddiepp06@gmail.com>
2024-12-05 14:39:20 -0400

    feat: Adding VFX to the robot

--- Assets/TestViewer/Scripts/RobotHandler.cs
+++ Assets/TestViewer/Scripts/RobotHandler.cs
@@ -4,8 +4,12 @@ namespace TestViewer
 {
     public class RobotHandler : MonoBehaviour
     {
+        #region VARIABLES
+        [SerializeField] ParticleSystem smokeLeft, smokeRight;
         [SerializeField] AudioSource audioSource;
+        #endregion
 
+        #region FUNCTIONS
         /// <summary>
         /// Play the bionic clip.
         /// </summary>
@@ -13,5 +17,20 @@ namespace TestViewer
         {
             audioSource.Play();
         }
+
+        /// <summary>
+        /// Start the smoke emission effect.
+        /// </summary>
+        private void StartSmokeEmission()
+        {
+            smokeLeft.Play();
+            smokeRight.Play();
+        }
+
+        private void Start()
+        {
+            Invoke(nameof(StartSmokeEmission), 25);
+        }
+        #endregion
     }
 }
